cfc3e5dccd60cbd645e89b093b2590573ecdcf16
Chekan <ky.chekan@gmail.com>
2025-06-01 16:34:04 +0300

    404

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,13 @@
 import css from "./NotFoundPage.module.css";
-import { MdErrorOutline } from "react-icons/md";
+import { PiFilmSlateDuotone } from "react-icons/pi";
 
 export default function NotFoundPage() {
   return (
     <div className={css.container}>
-      <MdErrorOutline className={css.icon} />
+      <PiFilmSlateDuotone className={css.icon} />
       <h3 className={css.text}>
-        Something went wrong with server! Please refresh the page or try again
-        later
+        A little support at the movie theater. Our server went to get popcorn.
+        He'll be back soon!
       </h3>
     </div>
   );
